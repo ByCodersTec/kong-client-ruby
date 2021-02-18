14f06f4e46c88829d88821451668516dfa2e0997
Raphael Cordeiro <phalado@gmail.com>
2021-02-18 18:51:04 -0300

    Correct a hash attribute

--- lib/kong/jwt.rb
+++ lib/kong/jwt.rb
@@ -15,9 +15,10 @@ module Kong
     end
 
     def delete
-      except_consumer_id_hash = attributes.reject { |k, v| k == 'consumer_id' && k == 'id' }
-      id = attributes["id"]
-      response = client.delete("#{@api_end_point}/#{id}", except_consumer_id_hash)
+      except_consumer_id_hash = attributes.reject { |k, v| k == 'consumer_id' }
+      except_id_hash = attributes.reject { |k, v| k == 'id' }
+      # id = attributes["id"]
+      response = client.delete("#{@api_end_point}/#{except_id_hash}", except_consumer_id_hash)
 
       init_attributes(response)
       self
