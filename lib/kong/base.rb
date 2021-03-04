ca600eefaf28900cf4dc85f06d432b85fb951677
Raphael Cordeiro <phalado@gmail.com>
2021-03-04 14:57:53 -0300

    Change api url to a fix value

--- lib/kong/base.rb
+++ lib/kong/base.rb
@@ -66,7 +66,7 @@ module Kong
       end
 
       base.send(:define_method, :init_api_end_point) do
-        @api_end_point = base::API_END_POINT
+        @api_end_point = "/admin-api#{base::API_END_POINT}"
       end
     end
 
