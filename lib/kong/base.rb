fe5c8205aa0110c04e13478c74d98c15d291c4cc
Raphael Cordeiro <phalado@gmail.com>
2021-03-04 15:20:01 -0300

    Change api url to a fix value

--- lib/kong/base.rb
+++ lib/kong/base.rb
@@ -66,8 +66,9 @@ module Kong
       end
 
       base.send(:define_method, :init_api_end_point) do
-        path = Kong::Client.http_client[:path]
-        p path
+        path =  Kong::Client.http_client.params[:path] ?
+                  Kong::Client.http_client.params[:path] :
+                  ''
         @api_end_point = "#{path}#{base::API_END_POINT}"
       end
     end
